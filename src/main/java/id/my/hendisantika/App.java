c7512e6bb22b31921ebb9fe3aa7f18b37a394b70
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 09:43:30 +0700

    Add PSVM🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/App.java
+++ src/main/java/id/my/hendisantika/App.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
  * Hello world!
  */
 public class App {
+    public static void main(String[] args) throws Exception {
+        try (ExecutorService executorThreadPool = Executors.newFixedThreadPool(100)) {
+            executeTasks(executorThreadPool);
+        }
+        try (ExecutorService executorVirtualThread = Executors.newVirtualThreadPerTaskExecutor()) {
+            executeTasks(executorVirtualThread);
+        }
+    }
+
     private static void executeTasks(ExecutorService executor) throws InterruptedException, ExecutionException {
         List<Task> tasks = new ArrayList<>();
         for (int i = 0; i < 1_000; i++) {
