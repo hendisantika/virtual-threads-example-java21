31f5ba060448ad0c79b4d0e3312eb97212e2ac0b
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 09:49:42 +0700

    Add thatCallReturnsResult🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/AppTest.java
+++ src/test/java/id/my/hendisantika/AppTest.java
@@ -1,34 +1,18 @@
 package id.my.hendisantika;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    void thatCallReturnsResult() throws Exception {
+        Task task = new Task(5);
+        Integer result = task.call();
+        assertNotNull(result);
     }
 }
