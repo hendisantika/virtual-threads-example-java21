cfd7e04110995751a1d1a1056aa75032a2e60f06
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 09:42:59 +0700

    Add executeTasks🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/App.java
+++ src/main/java/id/my/hendisantika/App.java
@@ -1,10 +1,30 @@
 package id.my.hendisantika;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static void executeTasks(ExecutorService executor) throws InterruptedException, ExecutionException {
+        List<Task> tasks = new ArrayList<>();
+        for (int i = 0; i < 1_000; i++) {
+            tasks.add(new Task(i));
+        }
+        long time = System.currentTimeMillis();
+        List<Future<Integer>> futures = executor.invokeAll(tasks);
+        long sum = 0;
+        for (Future<Integer> future : futures) {
+            sum += future.get();
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("****************************************************");
+        System.out.println("sum = " + sum + "; time = " + time + " ms");
+        System.out.println("****************************************************\n\n");
+        executor.shutdown();
     }
 }
